eb1a96d312a454b0da9362dc6be33a0675bd282b
hao2365 <1290657248@qq.com>
2019-11-11 15:48:14 +0800

    拦截器修改

--- src/main/java/com/whtt/cellingprice/config/WebAppConfig.java
+++ src/main/java/com/whtt/cellingprice/config/WebAppConfig.java
@@ -4,18 +4,18 @@ import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.annotation.Resource;
 
 @Configuration
-public class WebAppConfig extends WebMvcConfigurationSupport {
+public class WebAppConfig implements WebMvcConfigurer {
     @Autowired
     private InterceptorConfig interceptorConfig;
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-
         //注册自定义拦截器，添加拦截路径和排除拦截路径
-        registry.addInterceptor(interceptorConfig).addPathPatterns("/api/**").excludePathPatterns("/sysUser/login","/sysUser/forgetPassword");
+        registry.addInterceptor(interceptorConfig).addPathPatterns("/**").excludePathPatterns("/sysUser/login","/sysUser/forgetPassword","/static/**");
     }
 
 }
